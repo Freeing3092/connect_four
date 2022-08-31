52fb55564f218a0233e242e448fdd77956b5e9ad
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2022-08-31 00:56:22 -0400

    test/feat: Turn valid input length and character methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,17 @@
 class Turn
-  def initialize()
+  attr_reader :board, :player1, :player2
+  def initialize(board, player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @board = board
   end
 
-
+  def valid_input_length(move)
+    move.length == 1
+  end
+  
+  def valid_input_character(letter)
+    ((letter.is_a?(Integer) == false) && (@board.game_board.keys).any?(letter.upcase))
+    #if false, return a message to player...input valid letter
+  end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,14 +3,45 @@ require './lib/player'
 require './lib/turn'
 
 RSpec.describe Turn do
-  let(:player) { Player.new('George', 'X') }
-  let(:computer) {Player.new('Hal', 'O')}
+  let(:player1) { Player.new('George', 'X') }
+  let(:player2) {Player.new('Hal', 'O')}
   let(:board) {Board.new}
-  let(:game) { Turn.new}
+  let(:turn) {Turn.new(board,player1,player2)}
 
   it 'should be an instance of a Turn class'do
-    expect(game).to be_a Turn
+    expect(turn).to be_a Turn
   end
 
+  it 'should recieve a single input from players'do
+    move = "ABBA"
+    expect(turn.valid_input_length(move)).to eq false
+    move = "V0"
+    expect(turn.valid_input_length(move)).to eq false
+    move = ""
+    expect(turn.valid_input_length(move)).to eq false
+    move = "*"
+    expect(turn.valid_input_length(move)).to eq true
+    move = "h"
+    expect(turn.valid_input_length(move)).to eq true
+  end
+
+  it 'should accept input only if it matches the board keys' do
+    letter = " "
+    expect(turn.valid_input_character(letter)).to eq false
+    letter = "*"
+    expect(turn.valid_input_character(letter)).to eq false
+    letter = "5"
+    expect(turn.valid_input_character(letter)).to eq false
+    letter = 5
+    expect(turn.valid_input_character(letter)).to eq false
+    letter = "á"
+    expect(turn.valid_input_character(letter)).to eq false
+    letter = "."
+    expect(turn.valid_input_character(letter)).to eq false
+    letter = "g"
+    expect(turn.valid_input_character(letter)).to eq true
+    letter = 'D'
+    expect(turn.valid_input_character(letter)).to eq true
+  end
 
 end
