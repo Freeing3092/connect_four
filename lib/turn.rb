9822ce85b4b77a00bc627f1948adccbe406ba41b
Rich Kaht <adamselene1776@protonmail.com>
2022-08-30 18:53:51 -0600

    Feature/Test- computer_move method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,16 @@
 class Turn
-  def initialize()
+  attr_reader :board, :player1, :player2, :move
+  def initialize(board, player1, player2)
+    @board = board
+    @player1 = player1
+    @player2 = player2
+    @move = move
   end
 
+  def computer_move
+    @move = @board.game_board.keys.sample
+    @board.game_board[move].push(player2.chip)
+  end
+  
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,10 +6,16 @@ RSpec.describe Turn do
   let(:player) { Player.new('George', 'X') }
   let(:computer) {Player.new('Hal', 'O')}
   let(:board) {Board.new}
-  let(:game) { Turn.new}
+  let(:game) { Turn.new(board, player, computer)}
 
   it 'should be an instance of a Turn class'do
     expect(game).to be_a Turn
+  end
+  
+  it "has a method for the computer move" do
+    game.computer_move
+    board.invert_board
+    expect(game.board.horizontal_board[0].join).to eq('O')
   end
 
 
