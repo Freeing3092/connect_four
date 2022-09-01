eeb62b747b6ab43b3e19a677ce2017de62238537
Rich Kaht <adamselene1776@protonmail.com>
2022-09-01 17:42:23 -0600

    Feature: update to initialize the game with a board and 2 players

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,14 @@
+require './lib/board'
+require './lib/player'
+require './lib/turn'
+
 class Game
   attr_reader :board, :player1, :player2
   
-  def initialize(board, player1, player2)
-    @board = board
-    @player1 = player1
-    @player2 = player2
+  def initialize()
+    @board = Board.new
+    @player1 = Player.new('Player', 'X')
+    @player2 = Player.new('Hal', 'O')
   end
+  
 end
