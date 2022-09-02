c50c2663e825d32813ddf2925e71344c6d0fb5e1
Rich Kaht <adamselene1776@protonmail.com>
2022-09-01 22:14:06 -0600

    Feature: Add methods for play and end_game

--- lib/game.rb
+++ lib/game.rb
@@ -3,12 +3,14 @@ require './lib/player'
 require './lib/turn'
 
 class Game
-  attr_reader :board, :player1, :player2
+  attr_reader :board, :player1, :player2, :winner, :turn
   
   def initialize()
     @board = Board.new
     @player1 = Player.new('Player', 'X')
     @player2 = Player.new('Hal', 'O')
+    @winner = winner
+    @turn = Turn.new(@board, @player1, @player2)
   end
   
   def win?(board_orientation, player)
@@ -20,10 +22,28 @@ class Game
         else
           player_consecutive_tokens = 0
         end
-        return true if player_consecutive_tokens >= 4
+        if player_consecutive_tokens >= 4
+          @winner = player
+          return true
+        end
       end
     end
     return false
   end
   
+  def play
+    until win?(board.game_board.values, @player1) || win?(board.game_board.values, @player2) || win?(board.horizontal_board.values, @player1) || win?(board.horizontal_board.values, @player2) || turn.draw?
+      @turn.player1_move
+      @turn.computer_move
+    end
+    end_game
+  end
+  
+  def end_game
+    if !@turn.draw?
+      puts "*~*~* #{winner.name} has won the game! *~*~*"
+    elsif @turn.draw?
+      puts "----- Tie game -----"
+    end
+  end
 end
