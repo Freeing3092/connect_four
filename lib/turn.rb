c03fbffa884600301bb50e1459ff6f5c01916155
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-09-03 19:31:05 -0400

    feat: column_full_count_on_nil_bug-fixed...refactor needed

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,28 +9,63 @@ class Turn
   #Method evaluates and upcases valid input; 
 
   def valid_input_character?(move)
+    # require 'pry';binding.pry
     ((move.is_a?(Integer) == false) && ((@board.game_board).has_key?(move.upcase)))
+
+    # move.length == 1
       #****if false, return a message to player...input valid move * ******
   end
   
     # this method calls for the player to make a move, evaluates the move, 
     #and inputs valid moves onto the board
     
+  def is_move_invalid?(move)
+    #check for invalid 
+    while (valid_input_character?(move) == false) 
+      puts "That is an invalid input! Please select an open column between A-G."
+      move =  gets.chomp.upcase
+    end
+      
+    if (valid_input_character?(move)  == true ) 
+       (column_full?(move) == false)
+      # record_move_print_board(move, @player1)
+        while (valid_input_character?(move) == false ) || (column_full?(move) == true)
+          puts "That is an invalid input! Please select an open column between A-G."
+          move =  gets.chomp.upcase
+          # (valid_input_character?(move) == false) && move.length == 1
+      end
+
+    end
+    record_move_print_board(move, @player1)
+  end
+
   def player1_move
     if draw? == true #end of method for immediate response to conidtion
       return "The game is a draw!"
     end
 
     move =  gets.chomp.upcase
-    while valid_input_character?(move) == false
-    puts "That is an invalid input! Please select an open column between A-G."
-    move =  gets.chomp.upcase
-    end
-    while column_full?(move) == true #?????||  valid_input_character?(move) == true
-      puts "That column is full! Please select another column."
-      move =  gets.chomp.upcase # runner file... run prun
-    end
-    record_move_print_board(move, @player1) 
+    
+    is_move_invalid?(move)
+    # while valid_input_character?(move) == false
+    # puts "That is an invalid input! Please select an open column between A-G."
+    # move =  gets.chomp.upcase
+    # end
+
+    # while column_full?(move) == true
+      # require 'pry';binding.pry
+        # if valid_input_character?(move) == false
+          # puts "That column is full! Please select another column."
+          # move =  gets.chomp.upcase
+          # player1_move
+          # is_move_invalid?(move)
+        # else
+          # valid_input_character?(move) == true
+          # break
+        # end
+        # next
+    # end
+    # record_move_print_board(move, @player1) 
   end
 
   # Method to have the computer select a random (and not full) column
@@ -52,8 +87,8 @@ class Turn
 
   def column_full?(move)
     # require 'pry';binding.pry
-    board.game_board[move].count == 6
 
+    board.game_board[move].count == 6
   end
   
   # Method to check if the entire board is full.
